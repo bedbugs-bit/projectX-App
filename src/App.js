b5e19649268a7e30a1c80af7279cad5a04840efe
Joel Obowu <57295185+bedbugs-bit@users.noreply.github.com>
2023-12-08 20:31:39 +0300

    Edit about page

--- src/App.js
+++ src/App.js
@@ -1,97 +1,99 @@
-  import React from "react";
-  import backgroundImage from "./images/olivier-miche-iIg4F2IWbTM-unsplash.jpg";
+import React from "react";
+import backgroundImage from "./images/olivier-miche-iIg4F2IWbTM-unsplash.jpg";
 
-  function App() {
-    const appStyles = {
-      backgroundImage: `url(${backgroundImage})`,
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
-      minHeight: "100vh",
-      padding: "1em",
-    };
+function App() {
+  const appStyles = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    minHeight: "100vh",
+    padding: "1em",
+  };
 
-    const navStyles = {
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "space-around",
-      listStyle: "none",
-      padding: "1em",
-      backgroundColor: "#333",
-      color: "white",
-    };
+  const navStyles = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    listStyle: "none",
+    padding: "1em",
+    backgroundColor: "#333",
+    color: "white",
+  };
 
-    const h1Styles = {
-      fontSize: "4em",
-      color: "#333",
-    };
-    const h2Styles = {
-      fontSize: "2em",
-      color: "white",
-    };
+  const h1Styles = {
+    fontSize: "4em",
+    color: "#333",
+  };
+  const h2Styles = {
+    fontSize: "2em",
+    color: "white",
+  };
 
-    const pStyles = {
-      fontSize: "1.2em",
-      color: "#777",
-    };
+  const pStyles = {
+    fontSize: "1.2em",
+    color: "#777",
+  };
 
-    const divStyles = {
-      padding: "5em",
-      paddingTop: "5em",
-      paddingLeft: "20em",
-      paddingRight: "20em",
-    };
-    const buttonStyles = {
-      padding: "10px 20px",
-      fontSize: "1em",
-      backgroundColor: "grey",
-      color: "white",
-      border: "none",
-      borderRadius: "5px",
-      cursor: "pointer",
-    };
-    const linkStyles = {
-      color: "inherit",
-      textDecoration: "none",
-    };
+  const divStyles = {
+    padding: "5em",
+    paddingTop: "5em",
+    paddingLeft: "20em",
+    paddingRight: "20em",
+  };
+  const buttonStyles = {
+    padding: "10px 20px",
+    fontSize: "1em",
+    backgroundColor: "grey",
+    color: "white",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+  };
+  const linkStyles = {
+    color: "inherit",
+    textDecoration: "none",
+  };
 
-    return (
-      <div className="App" style={appStyles}>
-        <nav style={navStyles}>
-          <a href="#home" style={linkStyles}>
-            Home
-          </a>
-          <a href="#about" style={linkStyles}>
-            About
-          </a>
-          <a href="#contact" style={linkStyles}>
-            Contact
-          </a>
-          <a href="#service" style={linkStyles}>
-            Service
-          </a>
-          <a href="#header" style={linkStyles}>
-            Header
-          </a>
-        </nav>
+  return (
+    <div className="App" style={appStyles}>
+      <nav style={navStyles}>
+        <a href="#home" style={linkStyles}>
+          Home
+        </a>
+        <a href="#about" style={linkStyles}>
+          About
+        </a>
+        <a href="#contact" style={linkStyles}>
+          Contact
+        </a>
+        <a href="#service" style={linkStyles}>
+          Service
+        </a>
+        <a href="#header" style={linkStyles}>
+          Header
+        </a>
+      </nav>
 
-        <div style={divStyles}>
-          <h1 style={h1Styles}>Project X: A Simple, User-Friendly Platform</h1>
-          <p style={pStyles}>
-            This application has been developed in response to tackling the final
-            exam question for the App4080 course. The application is a simple and
-            a user-friendly platform. The application is built using ReactJS,
-            NodeJS.
-          </p>
-          <button style={buttonStyles}>Hello World</button>
-        </div>
-
-        <div id="about" style={{ padding: "10em", paddingTop: "3rem" }}>
-          <h2 style={h2Styles}>About</h2>
-          <p style={pStyles}>This is the about section.</p>
-        </div>
+      <div style={divStyles}>
+        <h1 style={h1Styles}>Project X: A Simple, User-Friendly Platform</h1>
+        <p style={pStyles}>
+          This application has been developed in response to tackling the final
+          exam question for the App4080 course. The application is a simple and
+          a user-friendly platform. The application is built using ReactJS,
+          NodeJS.
+        </p>
+        <button style={buttonStyles}>Hello World</button>
       </div>
-    );
-  }
 
-  export default App;
+      <div id="about" style={{ padding: "10em", paddingTop: "3rem" }}>
+        <h2 style={h2Styles}>About</h2>
+        <p style={pStyles}>
+          About Page, we discuss what is relevant to us and our community.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
